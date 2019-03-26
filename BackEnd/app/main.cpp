9f57ea2b82482aa3853b9963b6fbd226e84140f6
Vova Zabila <wzabila1995@gmail.com>
2019-03-26 22:37:11 +0200

    Add easyloggingpp in project.

--- BackEnd/app/main.cpp
+++ BackEnd/app/main.cpp
@@ -14,6 +14,10 @@ INITIALIZE_EASYLOGGINGPP
 int main(int argc, char *argv[]) {
   QGuiApplication app(argc, argv);
 
+  el::Configurations conf("log_settings.conf");
+  el::Loggers::reconfigureLogger("default", conf);
+  el::Loggers::reconfigureAllLoggers(conf);
+
   std::shared_ptr<ui::ViewModel> mpUI = std::make_shared<ui::ViewModel>();
   std::shared_ptr<server::Server> mpServer = std::make_shared<server::Server>();
   std::shared_ptr<ctrl::ServerController> mpServerCtrl =
